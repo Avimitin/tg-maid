b307e0b030d0643aade7bb471daecdd6f85d18ae
Avimitin <avimitin@gmail.com>
2022-07-18 16:29:29 +0800

    redis: implement all redis feature for currency cache
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/redis.rs
+++ src/redis.rs
@@ -2,29 +2,34 @@ use crate::currency::CurrenciesStorage;
 use redis::{aio::Connection, AsyncCommands};
 use std::collections::HashMap;
 
+const DATE_FORMAT: &str = "%Y-%m-%d-%H-%M-%S";
+const CODE_PREFIX_KEY: &str = "currency-code";
+const DATE_KEY: &str = "currency-last-update";
+
 #[async_trait::async_trait]
 impl CurrenciesStorage for Connection {
     async fn update(&mut self, codes: HashMap<String, String>) {
         for (k, v) in codes {
-            match self.hset("currency-code", k, v).await {
+            match self.hset(CODE_PREFIX_KEY, k, v).await {
                 Err(_e) => {}
                 Ok(()) => {}
             }
         }
 
-        match self
-            .set("currency-last-update", chrono::Utc::now().to_string())
-            .await
-        {
+        let date = chrono::Utc::now().format(DATE_FORMAT).to_string();
+        match self.set("currency-last-update", date).await {
             Err(_e) => {}
             Ok(()) => {}
         };
     }
 
     async fn is_outdated(&mut self) -> bool {
-        let last_date: Option<String> = self.get("currency-last-update").await.ok();
+        let last_date: Option<String> = self.get(DATE_KEY).await.ok();
         if let Some(last_date) = last_date {
-            todo!()
+            let date = chrono::NaiveDateTime::parse_from_str(&last_date, DATE_FORMAT)
+                .expect("Fail to parse date from redis cache");
+            let now = chrono::Utc::now().naive_utc();
+            return now - date > chrono::Duration::days(1);
         }
         false
     }
