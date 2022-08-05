b3fbb93274e234ecb0251e945dbc46343a1baf2d
Avimitin <avimitin@gmail.com>
2022-08-05 14:11:22 +0800

    request: separate request functionality into different trait
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,12 +1,28 @@
-pub mod collect;
-pub mod req;
-pub mod types;
-pub mod scraper;
-pub mod ksyx;
-pub mod archlinux;
+mod archlinux;
+mod collect;
+mod currency;
+mod ehentai;
+mod ksyx;
+mod nsfw;
+mod piggy;
+mod scraper;
+mod weather;
 
-pub mod redis;
+pub mod request {
+    pub use super::{
+        archlinux::ArchLinuxPacman, currency::CurrenciesFetcher, ehentai::EhentaiFetcher,
+        nsfw::NsfwContentFetcher, piggy::RecipeProvider, weather::WeatherReporter,
+    };
+}
 
-pub mod handlers;
-pub mod commands;
-pub mod runtime;
+pub mod cache {
+    pub use super::{
+        collect::CollectedMsgCache, currency::CurrenciesCache, ksyx::KsyxCounterCache,
+    };
+}
+
+pub mod prelude {
+    pub use super::request::*;
+    pub use super::cache::*;
+    pub use super::collect::MsgForm;
+}
--- src/modules/piggy.rs
+++ src/modules/piggy.rs
@@ -1,3 +1,44 @@
+use anyhow::Result;
+use rand::Rng;
+use scraper::{Html, Selector};
+
+fn collect_recipe(page: &str) -> Result<Vec<String>> {
+    let page = Html::parse_fragment(page);
+    let recipe_list = Selector::parse("ul.on").unwrap();
+    let li = Selector::parse("li a p").unwrap();
+
+    // take one
+    let ul = page
+        .select(&recipe_list)
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("Fail to select recipe from the given HTML page"))?;
+
+    let mut v = Vec::new();
+    for elem in ul.select(&li) {
+        if let Some(recipe) = elem.text().next() {
+            v.push(recipe.to_string());
+        }
+    }
+
+    if v.is_empty() {
+        anyhow::bail!("Can not find any recipe for cooking piggy")
+    }
+
+    Ok(v)
+}
+
+#[tokio::test]
+async fn test_select_recipe() {
+    let text = reqwest::get("https://www.meishichina.com/YuanLiao/ZhuRou/")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    dbg!(collect_recipe(&text).unwrap());
+}
+
 #[async_trait::async_trait]
 pub trait RecipeProvider {
     type Result;
@@ -15,6 +56,22 @@ impl RecipeProvider for crate::butler::Fetcher {
         ))
         .unwrap();
 
-        self.fetch(url).await
+        let page = self.fetch(url).await?;
+
+        // Deserialize HTML page is a heavy task, however we don't have async way to do
+        // it. So what I can do is just not let the job block current thread.
+        let task = move || -> String {
+            let res = collect_recipe(&page);
+            match res {
+                Ok(v) => {
+                    use rand::Rng;
+                    let choice: usize = rand::thread_rng().gen_range(0..v.len());
+                    format!("今天我们这样吃 piggy: {}", v[choice])
+                }
+                Err(e) => format!("今天没法吃 piggy 了呜呜呜: {e}"),
+            }
+        };
+
+        Ok(tokio::task::block_in_place(task))
     }
 }
