032297b3816022876657514098ff8d266a6af8de
Avimitin <avimitin@gmail.com>
2022-08-31 18:58:37 +0800

    bili: impl scraper
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/maid/watcher/bili.rs
+++ src/maid/watcher/bili.rs
@@ -3,15 +3,19 @@
 use std::{collections::HashMap, sync::Arc};
 
 use serde::Deserialize;
+use teloxide::prelude::*;
 
-const ENDPOINT: &str = "http://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids";
+const ENDPOINT: &str = "https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids";
 
 struct Runtime {
     c: reqwest::Client,
+    b: AutoSend<Bot>,
+    cfg: Config,
 }
 
-struct Config {
-    watch: Vec<u32>,
+pub struct Config {
+    pub watch: Vec<u32>,
+    pub notify: Vec<ChatId>,
 }
 
 type BoxStr = Box<str>;
@@ -35,20 +39,85 @@ struct RoomInfo {
     area_v2_name: BoxStr,
 }
 
+impl RoomInfo {
+    fn to_captions(&self) -> String {
+        format!(
+            "{} 开播了！\n直播标题: {}\n分区: #{}\n",
+            self.username, self.title, self.area_v2_name
+        )
+    }
+}
+
 async fn get_room_info(
-    rt: Arc<Runtime>,
+    client: &reqwest::Client,
     uids: &[u32],
 ) -> anyhow::Result<HashMap<BoxStr, RoomInfo>> {
     let payload = HashMap::from([("uids", uids)]);
-    let res =
-        rt.c.post(ENDPOINT)
-            .json(&payload)
-            .send()
-            .await?
-            .json::<Response>()
-            .await?;
+    let res = client
+        .post(ENDPOINT)
+        .json(&payload)
+        .send()
+        .await?
+        .json::<Response>()
+        .await?;
 
     Ok(res.data)
+}
+
+async fn watch_and_response(rt: Arc<Runtime>) {
+    let res = get_room_info(&rt.c, &rt.cfg.watch).await;
+    match res {
+        Ok(rooms) => {
+            for (_, room_info) in rooms {
+                for chat in &rt.cfg.notify {
+                    let cover = reqwest::Url::parse(&room_info.cover_from_user);
+                    if cover.is_err() {
+                        break;
+                    }
+                    let cover = cover.unwrap();
+
+                    if let Err(err) =
+                        rt.b.send_photo(*chat, teloxide::types::InputFile::url(cover))
+                            .caption(room_info.to_captions())
+                            .await
+                    {
+                        tracing::error!("fail to notify bili info: {err}")
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            tracing::error!("fail to get information from bilibili: {err}")
+        }
+    }
+}
+
+pub fn spawn_watcher(cfg: Config, b: AutoSend<Bot>) {
+    let (tx, rx) = tokio::sync::watch::channel(1_u8);
+    let mut heartbeat = tokio::time::interval(std::time::Duration::from_secs(60));
+    let rt = Arc::new(Runtime {
+        c: reqwest::Client::new(),
+        b,
+        cfg,
+    });
+
+    tokio::spawn(async move {
+        loop {
+            let mut rx = rx.clone();
+            let rt = Arc::clone(&rt);
+
+            tokio::select! {
+                _ = rx.changed() => {
+                    break;
+                }
+                _ = heartbeat.tick() => {
+                    watch_and_response(rt).await;
+                }
+            }
+        }
+    });
+
+    tokio::spawn(super::notify_on_ctrl_c(tx));
 }
 
 #[tokio::test]
--- src/maid/watcher/mod.rs
+++ src/maid/watcher/mod.rs
@@ -5,3 +5,9 @@ pub mod weibo;
 pub mod osu;
 
 pub mod bili;
+
+async fn notify_on_ctrl_c(tx: tokio::sync::watch::Sender<u8>) {
+    tokio::signal::ctrl_c().await.ok();
+    tracing::info!("quiting osu watcher...");
+    tx.send(0).expect("fail to send signal into osu watcher");
+}
