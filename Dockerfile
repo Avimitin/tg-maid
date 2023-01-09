21c1d3994099f0a1463c7404ad0635abf4f875ba
Avimitin <avimitin@gmail.com>
2023-01-09 15:13:14 +0800

    healthcheck: remove the HTTP server dependency
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ FROM debian:bullseye-slim
 COPY --from=build-env /src/butler/target/release/rusty-maid /bin/maid
 RUN apt-get update && apt-get install -y \
       --no-install-recommends \
-      curl \
+      netcat-openbsd \
       ca-certificates \
       && apt-get clean \
       && rm -rf /var/lib/apt/lists/*
-HEALTHCHECK CMD curl --insecure --silent --show-error --fail "http://localhost:11451/healthcheck" || exit 1
+HEALTHCHECK CMD nc -z 127.0.0.1 11451 || exit 1
 ENTRYPOINT ["/bin/maid"]
--- src/maid/listen.rs
+++ src/maid/listen.rs
@@ -1,24 +1,36 @@
-use warp::Filter;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
 /// Spawn a health check listener in a non-blocking task for Docker HEALTHCHECK task
 pub fn spawn_healthcheck_listner(port: u16) {
     // FIXME: This is an orphan task!
     tokio::task::spawn(async move {
-        let heartbeat = warp::path("healthcheck").map(|| "bong bong");
-        warp::serve(heartbeat).run(([127, 0, 0, 1], port)).await;
+        let listener = TcpListener::bind(("127.0.0.1", port))
+            .await
+            .expect("fail to bind docker health listener");
+
+        tracing::info!("Docker health check listening on port {port}");
+
+        while let Ok((mut stream, _)) = listener.accept().await {
+            tracing::debug!("New Stream Incoming");
+            let res = stream.write_all(b"OK").await;
+            if let Err(err) = res {
+                tracing::error!("fail to response to health checker: {err}")
+            }
+        }
     });
 }
 
 #[tokio::test]
 async fn test_healthcheck() {
     spawn_healthcheck_listner(11451);
-    assert_eq!(
-        "bong bong",
-        reqwest::get("http://localhost:11451/healthcheck")
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap()
-    );
+
+    tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
+
+    use tokio::process::Command;
+    let op = Command::new("nc")
+        .args(["127.0.0.1", "11451"])
+        .output()
+        .await
+        .expect("fail to execute netcat");
+    assert_eq!("OK", String::from_utf8(op.stdout).unwrap());
 }
