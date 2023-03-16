3fc930108809d6cbdbc1fa95a8471a3515aab149
Avimitin <dev@avimit.in>
2023-03-16 17:53:54 +0800

    Add konachan image fetcher
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -52,4 +52,39 @@ impl HttpClient {
     }
 }
 
+/// Represent the konachan API response json
+#[derive(Deserialize, Debug)]
+pub struct KonachanApiResponse {
+    pub jpeg_url: String,
+    pub file_url: String,
+    pub file_size: u32,
+    pub author: String,
+}
+
 // TODO: impl DataFetcher for HttpClient {}
+impl HttpClient {
+    const KONACHAN_LINK: &str = "https://konachan.com/post.json?limit=200&tags=%20rating:explicit";
+
+    async fn fetch_nsfw_anime_img(&self) -> anyhow::Result<Sendable> {
+        let link = reqwest::Url::parse(Self::KONACHAN_LINK).unwrap();
+
+        let response: Vec<KonachanApiResponse> = self
+            .to_t(link)
+            .await
+            .with_context(|| "fail to get resp from konachan API")?;
+
+        let mut choice = rand::thread_rng();
+        let choice = choice.gen_range(0..response.len());
+        let response = &response[choice];
+
+        Ok(Sendable::from_url_and_caption(
+            &response.jpeg_url,
+            format!(
+                "<a href=\"{}\">Download Link</a>\nSize: {:.2} MB, Author: {}",
+                response.file_url,
+                response.file_size as f32 / 1000000.0,
+                response.author
+            ),
+        ))
+    }
+}
