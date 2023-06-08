8de2cab3742cd6cffc9fbf16240a24279d42e148
Avimitin <dev@avimit.in>
2023-06-08 23:44:32 +0800

    Try find the largest avatar

--- build.rs
+++ build.rs
@@ -1,21 +1,12 @@
-use fontconfig::Fontconfig;
+use std::path;
 
 fn main() {
-    let fc = Fontconfig::new().expect("fail to initialize font config");
-    let sans_light = fc
-        .find("Noto Sans CJK SC", Some("style=light"))
-        .expect("Font Noto Sans CJK SC Light not found");
-    let sans_bold = fc
-        .find("Noto Sans CJK SC", Some("style=bold"))
-        .expect("Font Noto Sans CJK SC Bold not found");
+    let bold = path::Path::new("./assets/NotoSansCJK-Bold.ttc");
+    let thin = path::Path::new("./assets/NotoSansCJK-Light.ttc");
 
-    println!(
-        "cargo:rustc-env=SANS_LIGHT_PATH={}",
-        sans_light.path.as_os_str().to_str().unwrap()
-    );
+    assert!(bold.exists());
+    assert!(thin.exists());
 
-    println!(
-        "cargo:rustc-env=SANS_BOLD_PATH={}",
-        sans_bold.path.as_os_str().to_str().unwrap()
-    );
+    println!("cargo:rustc-env=SANS_LIGHT_PATH={}", thin.to_str().unwrap());
+    println!("cargo:rustc-env=SANS_BOLD_PATH={}", bold.to_str().unwrap());
 }
