bd19727cdbf964f77eb153c0f56d2f07e632a965
Avimitin <dev@avimit.in>
2023-07-17 01:22:00 +0800

    Setup config when building image
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/docker-image.nix
+++ nix/docker-image.nix
@@ -2,6 +2,31 @@
 let
   # Bot links to openssl at build time
   LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
+
+  bot_config = pkgs.writeTextFile {
+    name = "config.toml";
+    text = ''
+      bot_token = "abcd"
+      redis_addr = "redis://localhost:6379"
+      log_level = "INFO"
+      health_check_port = 11451
+
+      [deepl]
+      api_key = "abcd"
+
+      [osu]
+      client_id = 0
+      client_secret = "abcd"
+
+      [bili_live_room_event]
+
+      [osu_user_activity_event]
+    '';
+  };
+
+  # Specify a dir for user to easily mount volume
+  workdir = "/app";
+  bot_cfg_path = "${workdir}/config.toml";
 in
 pkgs.dockerTools.streamLayeredImage {
   name = "ghcr.io/Avimitin/tg-maid";
@@ -15,8 +40,17 @@ pkgs.dockerTools.streamLayeredImage {
      my-maid-pkg
   ];
 
+  runAsRoot = ''
+    #!${pkgs.runtimeShell}
+    mkdir -p ${workdir}
+    cp ${bot_config} ${bot_cfg_path}
+  '';
+
   config = {
-    env = [ ''LD_LIBRARY_PATH=${LD_LIBRARY_PATH}'' ];
+    env = [
+      "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+      "TG_MAID_CFG_PATH=${bot_cfg_path}"
+    ];
     cmd = [ "${tg-maid}/bin/tgbot" ];
     healthcheck = {
       test = [
