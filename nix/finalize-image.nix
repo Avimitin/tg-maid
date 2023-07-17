a604d063403aac0d86e2f5843c3b5359afe95a04
Avimitin <dev@avimit.in>
2023-07-17 22:35:49 +0800

    Rewrite the old docker image builder by Nix
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ nix/finalize-image.nix
@@ -0,0 +1,24 @@
+{ pkgs, do_push ? false, is_latest ? false, name, tag }:
+with pkgs.lib.strings;
+
+# Load tarball into docker images
+pkgs.writeScript "finalize-docker-image" (''
+  set -e; set -o pipefail
+  # Try verify docker exists and daemon is reachable
+  docker info
+
+  nix build --print-out-paths '.#docker' \
+  | docker load --quiet
+'' +
+  # If this is the latest build, add new tag for it
+  optionalString is_latest ''
+    docker image tag ${name}:${tag} ${name}:latest
+  '' +
+  # If we need to run the push
+  optionalString do_push ''
+    docker push ${name}:${tag}
+  '' +
+  # If we need to run the push and it is the latest build
+  optionalString (do_push && is_latest) ''
+    docker push ${name}:latest
+  '')
